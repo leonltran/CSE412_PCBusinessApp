5a971ad4a3e2061aad074766169229f6de76e436
leonltran <leonltran.cs@gmail.com>
2023-12-01 21:13:48 -0700

    install Npgsql package, add some UI to frmAddCustomer

--- frmAddCustomer.Designer.cs
+++ frmAddCustomer.Designer.cs
@@ -29,19 +29,109 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmAddCustomer));
+            lblName = new Label();
+            txtf = new TextBox();
+            textBox1 = new TextBox();
+            label1 = new Label();
+            textBox2 = new TextBox();
+            label2 = new Label();
+            textBox3 = new TextBox();
+            label3 = new Label();
             SuspendLayout();
+            // 
+            // lblName
+            // 
+            lblName.AutoSize = true;
+            lblName.Location = new Point(12, 9);
+            lblName.Name = "lblName";
+            lblName.Size = new Size(39, 15);
+            lblName.TabIndex = 0;
+            lblName.Text = "Name";
+            // 
+            // txtf
+            // 
+            txtf.Location = new Point(12, 27);
+            txtf.Name = "txtf";
+            txtf.Size = new Size(376, 23);
+            txtf.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(12, 159);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(376, 23);
+            textBox1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 141);
+            label1.Name = "label1";
+            label1.Size = new Size(39, 15);
+            label1.TabIndex = 2;
+            label1.Text = "Name";
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(12, 115);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(376, 23);
+            textBox2.TabIndex = 5;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(12, 97);
+            label2.Name = "label2";
+            label2.Size = new Size(39, 15);
+            label2.TabIndex = 4;
+            label2.Text = "Name";
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(12, 71);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(376, 23);
+            textBox3.TabIndex = 7;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(12, 53);
+            label3.Name = "label3";
+            label3.Size = new Size(39, 15);
+            label3.TabIndex = 6;
+            label3.Text = "Name";
             // 
             // frmAddCustomer
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(400, 450);
+            Controls.Add(textBox3);
+            Controls.Add(label3);
+            Controls.Add(textBox2);
+            Controls.Add(label2);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+            Controls.Add(txtf);
+            Controls.Add(lblName);
             Icon = (Icon)resources.GetObject("$this.Icon");
             Name = "frmAddCustomer";
             Text = "Add New Customer";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label lblName;
+        private TextBox txtf;
+        private TextBox textBox1;
+        private Label label1;
+        private TextBox textBox2;
+        private Label label2;
+        private TextBox textBox3;
+        private Label label3;
     }
 }
